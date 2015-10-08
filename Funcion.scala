5d2f10d3f338db3ddbb1fc796877ad1dc9cb6271
Cesar Mijangos <camf.eklahana04@hotmail.com>
2015-10-07 22:59:33 -0500

    Arreglado el ejercicio 1 con su funcion

--- Funcion.scala
+++ Funcion.scala
@@ -1,34 +1,46 @@
-Def calificacion () = {
+def calificacion () = {
 println ("Calificación de algoritmos.")
 println ("Ingresar los datos que se piden por favor.")
+println ("   ")
 
 println ("Primer parcial: ")
-	val cal1 = readFloat ()
+	val cal1 = readFloat()
 println ("Segundo parcial: ")
-	val cal2 = readFloat ()
+	val cal2 = readFloat()
 println ("Tercer parcial: ")
-	val cal3 = readFloat ()
+	val cal3 = readFloat()
+
 	val promedio = (cal1 + cal2 + cal3)/3
 	val porpromedio = promedio * 5.5
-println ("El porcentaje del promedio es: " +porpromedio)
 
+println ("   ")
+println ("El porcentaje del promedio es: " +porpromedio+ " %")
+
+println ("   ")
 println ("Calificación del examen final: ")
-	val calfin = readFloat ()
+	val calfin = readFloat()
 	val porcalfin = calfin * 3
-println ("El porcentaje del examen final es: " +porcalfin)
 
+println ("   ")
+println ("El porcentaje del examen final es: " +porcalfin+ " %")
+
+println ("   ")
 println ("Calificación del trabajo final: ")
-	val caltra = readFloat ()
+	val caltra = readFloat()
 	val porcaltra = caltra * 1.5
-println ("El porcentaje del trabajo final es: " +porcaltra)
 
-	val final = porpromedio + porcalfin + porcaltra
-println ("El porcentaje final es: " +final)
+println ("   ")
+println ("El porcentaje del trabajo final es: " +porcaltra+ " %")
+
+	val fin = (porpromedio + porcalfin + porcaltra)
+println ("   ")
+println ("El porcentaje final es: " +fin+ " %")
 }
 
-Def hospital () = {
+def hospital () = {
 println ("Hospital.")
 println ("Ingresar los datos que se piden por favor.")
+println ("   ")
 
 println ("Presupuesto anual: ")
 	val preanual = readFloat ()
@@ -37,14 +49,16 @@ println ("Presupuesto anual: ")
 	val pretrau = preanual * 0.3
 	val preped = preanual * 0.3
 
-println ("El porcentaje de Ginecología es: " +pregine)
-println ("El porcentaje de Traumatología es: " +pretrau)
-println ("El porcentaje de Pediatría es: " +preped)
+println ("   ")
+println ("El porcentaje de Ginecología es: " +pregine+ " %")
+println ("El porcentaje de Traumatología es: " +pretrau+ " %")
+println ("El porcentaje de Pediatría es: " +preped+ " %")
 }
 
-Def empresa () = {
+def empresa () = {
 println ("Empresa.")
 println ("Ingresar los datos que se piden por favor.")
+println ("   ")
 
 println ("Inversión de la primera persona: ")
 	val inv1 = readFloat ()
@@ -54,22 +68,25 @@ println ("Inversión de la tercera persona: ")
 	val inv3 = readFloat ()
 
 	val invtotal = inv1 + inv2 + inv3
+println ("   ")
 println ("Inversión total: " +invtotal)
 
 	val porinv1 = (inv1 * 100) / invtotal
 	val porinv2 = (inv2 * 100) / invtotal
 	val porinv3 = (inv3 * 100) / invtotal
 
-println ("El porcentaje de la primera persona es: " +porinv1)
-println ("El porcentaje de la segunda persona es: " +porinv2)
-println ("El porcentaje de la tercera persona es: " +porinv3)
+println ("   ")
+println ("El porcentaje de la primera persona es: " +porinv1+ " %")
+println ("El porcentaje de la segunda persona es: " +porinv2+ " %")
+println ("El porcentaje de la tercera persona es: " +porinv3+ " %")
 }
 
 println ("¿Cuál ejercicio desea ejecutar? ")
 println ("1) Calificación. ")
-println ("2) Presupuesto. ")
-println ("3) Porcentaje. ")
-val opcion = readInt ()
+println ("2) Presupuesto de un hospital. ")
+println ("3) Inversión de una empresa. ")
+val option = readInt ()
+println ("   ")
 
 option match {
 
