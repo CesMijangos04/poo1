6daaa7686717c5878325b882134e4440a377990c
Cesar Mijangos <camf.eklahana04@hotmail.com>
2015-10-07 22:36:18 -0500

    Arreglado los ejercicios

--- Jubilacion.scala
+++ Jubilacion.scala
@@ -1,20 +1,22 @@
-printIn ("IMSS.")
-printIn ("Ingresar los datos que se piden por favor.")
+println ("IMSS.")
+println ("Ingresar los datos que se piden por favor.")
+println ("   ")
 
 println ("¿Cuál es tu edad?")
 	val ed = readInt ()
 
 println ("¿Qué antigüedad tienes en tu empleo?")
 	val anti = readInt ()
+println ("   ")
 
 if ( ed >= 60 && anti < 25 ) {
-  println ("El tipo de jubilación es por: Edad".)
+  println ("El tipo de jubilación es por: Edad.")
   }
   if ( ed < 60 && anti >= 25 ) {
   	println ("El tipo de jubilación es por: Antigüedad joven.")
 	  }
 	  if( ed >= 60 && anti >= 25 ) {
-  		println ("Tu tipo de jubilación es por: Antigüedad alta.")
+  		println ("Tu tipo de jubilación es por: Antigüedad adulta.")
 		  }
 		  else {
 		    println ("Gracias.")
--- Tienda.scala
+++ Tienda.scala
@@ -1,31 +1,33 @@
-printIn ("Tienda.")
-printIn ("Ingresar los datos que se piden por favor.")
+println ("Tienda.")
+println ("Ingresar los datos que se piden por favor.")
+println ("   ")
 
-printIn ("Total de compra: ")
+println ("Total de compra: ")
 	val tocom = readFloat ()
-printIn ("Color de bolita que sacó (descuento): ")
+println ("Color de bolita que sacó (descuento): ")
 	val color = readLine ()
+println ("   ")
 
-if (color == "blanca") {
+if (color == "blanca" || color == "blanco") {
 	println("Total a pagar: " +tocom)
 	}
 
 if (color == "verde") {
 	val desverde = (tocom - (tocom * .1))
-	println("Total a pagar:" *desverde)
+	println("Total a pagar: " +desverde)
 	}
 
-if (color == "amarillo") {
+if (color == "amarilla" || color == "amarillo") {
 	val desamarillo = (tocom - (tocom * .25))
-	println("Total a pagar:" *desamarillo)
+	println("Total a pagar: " +desamarillo)
   	}
 
 if (color == "azul") {
 	val desazul = (tocom - (tocom * .5))
-	println("Total a pagar:" *desazul)
+	println("Total a pagar: " +desazul)
  	}
 
-if (color == "roja") {
+if (color == "roja" || color == "rojo") {
 	val desroja = (tocom - (tocom * 1))
-	println("Total a pagar:" *desroja)
+	println("Total a pagar: " +desroja)
 	}
